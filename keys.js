72b5370c938215e71d3b1a84b923ea2b670f59fa
pdelao512 <pdelao512@gmail.com>
2017-07-24 22:01:25 -0500

    files added

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<fZyvMus6auiXoLhrw4Q9c0dOo>',
+  consumer_secret: '<GPeatAVoK7V0lDK7k15fNEdNs2cTLBDlVlgN8f1gqVEXvEU2UQ>',
+  access_token_key: '<889662453990162434-vFW9xXeJrnRva0kl1w0G3sK3qtKI1mz>',
+  access_token_secret: '<QaXehsI4PsVeSGageUsewaKeOaLmHbAVtzw0CiGk3s0AlAccess>',
+}
